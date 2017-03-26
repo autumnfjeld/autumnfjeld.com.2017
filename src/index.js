45c82369713eb92dadb0d9e7171031721b035715
Autumn Fjeld <autumn@talentbin.com>
2017-03-26 15:09:56 -0700

    Convert index.html, head, links, scripts and nav section to pug

---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+var general = {
+    firstName: 'Autumn',
+    lastName: 'Fjeld',
+    email: 'autumnfjeld@gmail.com',
+    careerTitle: 'Software Engineer / Web Developer',
+    resumeLink: 'img/Autumn_Fjeld_CV_20170311.pdf'
+};
+
+var nav = {
+
+};
+
+var portfolio = {
+    title1: 'This is title1',
+    title2: 'title2',
+    img: {
+        primary: 'img/portfolio-item-zoom.jpg',
+        thumb:'img/portfolio-item-thumb.jpg',
+        title: 'Image title'
+    }
+};
+
+exports.app = {
+    general: general,
+    portfolio: portfolio
+};
