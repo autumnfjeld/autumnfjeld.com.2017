2e956e92cc3f4a771b551390ee5419fe7f5906bc
Autumn Fjeld <autumn@talentbin.com>
2017-04-14 19:06:40 -0700

    Convert intro to pug

--- src/index.js
+++ src/index.js
@@ -4,7 +4,30 @@ var general = {
     lastName: 'Fjeld',
     email: 'autumnfjeld@gmail.com',
     careerTitle: 'Software Engineer / Web Developer',
-    resumeLink: 'img/Autumn_Fjeld_CV_20170311.pdf'
+    resumeLink: 'img/Autumn_Fjeld_CV_20170311.pdf',
+    webProfiles: {
+        linkedIn: {
+            url: 'https://www.linkedin.com/in/autumnfjeld',
+            icon: 'fa-linkedin'
+        },
+        twitter: {
+            url: 'https://twitter.com/autaut',
+            icon: 'fa-twitter'
+        },
+        github: {
+            url: 'https://github.com/autumnfjeld',
+            icon: 'fa-github'
+        },
+        currentJob: {
+            url: 'https://www.talentbin.com/team',
+            icon: 'fa-user'
+        },
+        email: {
+            url: 'mailto:autumnfjeld@gmail.com',
+            icon: 'fa-paper-plane-o'
+        }
+    },
+    introBlurb: 'My dream job is working at the intersection of logical software development and creative design while driving development of an intuitive user interface <i>for the user</i>. Lucky that my day job is coding for <strong> visual pleasure </strong>'
 };
 
 var nav = {
